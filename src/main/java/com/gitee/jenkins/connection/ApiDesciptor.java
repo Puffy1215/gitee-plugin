343c1ed36ea1542722bc2da0f95b5955720511ee
Andy Xie <andy.xie.opensource@gmail.com>
2026-05-07 22:48:46 +0800

    Permission check and requre POST

--- src/main/java/com/gitee/jenkins/connection/ApiDesciptor.java
+++ src/main/java/com/gitee/jenkins/connection/ApiDesciptor.java
@@ -1,15 +1,23 @@
 package com.gitee.jenkins.connection;
 
+import org.kohsuke.stapler.interceptor.RequirePOST;
+
+import hudson.security.Permission;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 
 public interface ApiDesciptor {
 
+    @RequirePOST
     default ListBoxModel doFillGiteeApiRepoItems() {
-        GiteeApiRepoProperty.DescriptorImpl descriptor = (com.gitee.jenkins.connection.GiteeApiRepoProperty.DescriptorImpl) Jenkins
-            .get().getJobProperty("GiteeApiRepoProperty");
+        if (Jenkins.get().hasPermission(Permission.CONFIGURE)) {
+            GiteeApiRepoProperty.DescriptorImpl descriptor = (com.gitee.jenkins.connection.GiteeApiRepoProperty.DescriptorImpl) Jenkins
+                    .get().getJobProperty("GiteeApiRepoProperty");
 
-        return descriptor.doFillGiteeApiRepoItems();
+            return descriptor.doFillGiteeApiRepoItems();
+        } else {
+            return null;
+        }
     }
-    
+
 }
